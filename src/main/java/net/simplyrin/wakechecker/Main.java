d9b3d14d21986a0db24214d33d3d8cbdce34e4ee
SimplyRin <simplyrin.dev@gmail.com>
2019-06-08 15:49:18 +0900

    Add files via upload

---
+++ src/main/java/net/simplyrin/wakechecker/Main.java
@@ -0,0 +1,230 @@
+package net.simplyrin.wakechecker;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import lombok.Data;
+import net.md_5.bungee.config.Configuration;
+import net.simplyrin.config.Config;
+import net.simplyrin.rinstream.RinStream;
+import net.simplyrin.wakechecker.utils.Task;
+import net.simplyrin.wakechecker.utils.ThreadPool;
+
+/**
+ * Created by SimplyRin on 2019/06/08.
+ *
+ * Copyright (c) 2019 SimplyRin
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		new Main().run();
+	}
+
+	public void run() {
+		new RinStream();
+
+		Configuration config;
+
+		File file = new File("config.yml");
+		if (!file.exists()) {
+			try {
+				file.createNewFile();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+			config = Config.getConfig(file);
+
+			config.set("Monitoring.One.Name", "Task-1 PC");
+			config.set("Monitoring.One.IP", "192.168.0.1");
+			config.set("Monitoring.One.Port", 3389);
+			config.set("Monitoring.One.Timeout", 500);
+			config.set("Monitoring.One.Startup.Enabling", true);
+			config.set("Monitoring.One.Startup.Request.URL", "https://api.yourserver.net/hooks.php");
+			config.set("Monitoring.One.Startup.Request.Type", "GET");
+
+			config.set("Monitoring.One.Shutdown.Enabling", true);
+			config.set("Monitoring.One.Shutdown.Request.URL", "https://api.yourserver.net/hooks.php");
+			config.set("Monitoring.One.Shutdown.Request.Type", "POST");
+			config.set("Monitoring.One.Shutdown.Request.Headers", Arrays.asList("User-Agent:Mozilla/5.0"));
+			config.set("Monitoring.One.Shutdown.Request.Data", "message=Runtime: {time}");
+
+			Config.saveConfig(config, file);
+		}
+
+		config = Config.getConfig(file);
+
+		List<Task> tasks = new ArrayList<>();
+		for (String key : config.getSection("Monitoring").getKeys()) {
+			Task task = new Task();
+
+			task.setName(config.getString("Monitoring." + key + ".Name"));
+			task.setIp(config.getString("Monitoring." + key + ".IP"));
+			task.setPort(config.getInt("Monitoring." + key + ".Port"));
+			task.setTimeout(config.getInt("Monitoring." + key + ".Timeout"));
+
+			task.getStartup().setEnabling(config.getBoolean("Monitoring." + key + ".Startup.Enabling"));
+			task.getStartup().getRequest().setUrl(config.getString("Monitoring." + key + ".Startup.Request.URL"));
+			task.getStartup().getRequest().setType(config.getString("Monitoring." + key + ".Startup.Request.Type"));
+			task.getStartup().getRequest().setHeaders(config.getStringList("Monitoring." + key + ".Startup.Request.Headers"));
+			task.getStartup().getRequest().setData(config.getString("Monitoring." + key + ".Startup.Request.Data"));
+
+			task.getShutdown().setEnabling(config.getBoolean("Monitoring." + key + ".Shutdown.Enabling"));
+			task.getShutdown().getRequest().setUrl(config.getString("Monitoring." + key + ".Shutdown.Request.URL"));
+			task.getShutdown().getRequest().setType(config.getString("Monitoring." + key + ".Shutdown.Request.Type"));
+			task.getShutdown().getRequest().setHeaders(config.getStringList("Monitoring." + key + ".Shutdown.Request.Headers"));
+			task.getShutdown().getRequest().setData(config.getString("Monitoring." + key + ".Shutdown.Request.Data"));
+
+			tasks.add(task);
+		}
+
+		System.out.println("タスク監視を開始します。");
+
+		for (Task task : tasks) {
+			ThreadPool.run(() -> {
+				boolean bool = false;
+
+				while (true) {
+					boolean portOpen = task.isPortOpen();
+					if (bool != portOpen) {
+						bool = portOpen;
+
+						if (portOpen) {
+							if (task.getStartup().isEnabling()) {
+								task.getStartup().getRequest().postRequest();
+							}
+
+							task.setStartTime(new Date());
+							System.out.println(task.getName() + "(" + task.getIp() + ") のポートが開放されました (起動しました)");
+						} else {
+							task.getShutdown().getRequest().setTime(this.getTimeFromDate(task.getStartTime()));
+
+							if (task.getShutdown().isEnabling()) {
+								task.getShutdown().getRequest().postRequest();
+							}
+
+							System.out.println(task.getName() + "(" + task.getIp() + ") のポートが閉鎖されました (シャットダウンしました)");
+						}
+					}
+
+					try {
+						TimeUnit.SECONDS.sleep(5);
+					} catch (Exception e) {
+					}
+				}
+			});
+		}
+	}
+
+	public Time getTimeFromDate(Date createdTime) {
+		int[] units = { Calendar.YEAR, Calendar.MONTH, Calendar.DAY_OF_MONTH, Calendar.HOUR_OF_DAY, Calendar.MINUTE, Calendar.SECOND };
+		Date today = new Date();
+		int[] result = new int[units.length];
+
+		Calendar sCal = Calendar.getInstance();
+		Calendar tCal = Calendar.getInstance();
+
+		sCal.setTime(createdTime);
+		tCal.setTime(today);
+
+		for (int i = units.length - 1; i >= 0; i--) {
+			result[i] = tCal.get(units[i]) - sCal.get(units[i]);
+			if (result[i] < 0) {
+				tCal.add(units[i - 1], -1);
+				int add = tCal.getActualMaximum(units[i]);
+				result[i] += (units[i] == Calendar.DAY_OF_MONTH) ? add : add + 1;
+			}
+		}
+
+		Time time = new Time();
+
+		time.year = result[0];
+		time.month = result[1];
+		time.day = result[2];
+		time.hour = result[3];
+		time.minute = result[4];
+		time.second = result[5];
+
+		return time;
+	}
+
+	@Data
+	public static class Time {
+		int year;
+		int month;
+		int day;
+		int hour;
+		int minute;
+		int second;
+
+		@Override
+		public String toString() {
+			String uptime = "";
+
+			if (year > 0) {
+				uptime += year + "年";
+			}
+			if (month > 0) {
+				uptime += month + "ヶ月";
+			}
+			if (day > 0) {
+				uptime += day + "日 ";
+			}
+
+			if (hour > 0) {
+				uptime += hour + "時間";
+			}
+			if (minute > 0) {
+				uptime += minute + "分";
+			}
+			if (second > 0) {
+				uptime += second + "秒";
+			}
+
+			if (uptime.trim().length() == 0) {
+				uptime += "0秒";
+			}
+
+			return uptime;
+		}
+
+		public static Time now() {
+			Date date = new Date();
+			Time time = new Time();
+
+			time.setYear(date.getYear());
+			time.setMonth(date.getMonth());
+			time.setDay(date.getDay());
+			time.setHour(date.getHours());
+			time.setMinute(date.getMinutes());
+			time.setSecond(date.getSeconds());
+
+			return time;
+		}
+	}
+}
---
+++ src/main/java/net/simplyrin/wakechecker/utils/Task.java
@@ -0,0 +1,105 @@
+package net.simplyrin.wakechecker.utils;
+
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import lombok.Data;
+import net.simplyrin.httpclient.HttpClient;
+import net.simplyrin.wakechecker.Main.Time;
+
+/**
+ * Created by SimplyRin on 2019/06/08.
+ *
+ * Copyright (c) 2019 SimplyRin
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+@Data
+public class Task {
+
+	private String name;
+	private String ip;
+	private int port;
+	private int timeout;
+	private Startup startup = new Startup();
+	private Shutdown shutdown = new Shutdown();
+
+	private Date startTime;
+
+	public boolean isPortOpen() {
+		try {
+			Socket socket = new Socket();
+			socket.connect(new InetSocketAddress(ip, port), timeout);
+			socket.close();
+			return true;
+		} catch (Exception e) {
+		}
+		return false;
+	}
+
+	@Data
+	public static class Startup {
+		private boolean enabling;
+		private Request request = new Request();
+	}
+
+	@Data
+	public static class Shutdown {
+		private boolean enabling;
+		private Request request = new Request();
+	}
+
+	@Data
+	public static class Request {
+		private String url;
+		private String type;
+		private List<String> headers;
+		private String data;
+
+		public void setTime(Time time) {
+			url = url.replace("{time}", time.toString());
+			data = data.replace("{time}", time.toString());
+		}
+
+		public String postRequest() {
+			if (url == null) {
+				throw new RuntimeException("You need set URL!");
+			}
+
+			HttpClient httpClient = new HttpClient(url);
+
+			if (headers != null) {
+				for (String value : headers) {
+					httpClient.addHeader(value.split(":")[0].trim(), value.split(":")[1].trim());
+				}
+			}
+
+			if (data != null) {
+				SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+				httpClient.setData(data.replace("{now}", simpleDateFormat.format(new Date())));
+			}
+
+			return httpClient.getResult();
+		}
+	}
+
+}
